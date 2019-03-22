33040c3272c1956192a6274a0b2ae2c63ef1a246
Nathan DAMIE <nathan.damie@gmail.com>
2019-03-22 17:47:14 +0100

    feat: get user

---
+++ index.ts
@@ -0,0 +1,29 @@
+
+/**
+ * Return the User who match the given id
+ * @summary get user by id
+ * @author NathanDM
+ * @public
+ * @param {string} The id of the user we are looking for
+ * @return {User} The user corresponding to the given Id
+ * @throws {NotFoundException} if no user matches the given id
+ * @example
+ * // return { firstName: 'John', lastName: 'Do' }
+ * getUserById('12345');
+ */
+function fct(param) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+getUserById('');
